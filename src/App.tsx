92402be2f0598dd68ca5137bd5d6e8958c1cea25
Vladimir Stoikov <vspsmoke@gmail.com>
2025-10-24 22:53:58 +0300

    feat(NavBar): add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
-  return <h1>brain-exercise</h1>;
+  return (
+    <>
+      <h1>brain-exercise</h1>
+      <NavBar />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function NavBar() {
+  return (
+    <ul>
+      <li>Начать заново</li>
+      <li>Прошлое</li>
+      <li>Следующее</li>
+    </ul>
+  );
+}
