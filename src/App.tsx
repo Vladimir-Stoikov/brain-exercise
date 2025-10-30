b2c12a567d222953fadc73eaad8fe0e25a2baf68
Vladimir Stoikov <vspsmoke@gmail.com>
2025-10-30 20:44:28 +0300

    feat(Main.st/index.css) choose palette, setup main and body style

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Outlet } from 'react-router';
 import './App.css';
 import NavBar from './components/NavBar';
+import MainSt from './components/styled-components/MainSt';
 
 function App() {
   return (
-    <>
+    <MainSt>
       <h1>brain-exercise</h1>
       <NavBar />
       <section>
         <Outlet />
       </section>
-    </>
+    </MainSt>
   );
 }
 
---
+++ src/components/styled-components/MainSt.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+
+const MainSt = styled.main`
+height: auto;
+width: auto;
+background-color: var(--secondary);
+padding: 2rem;
+border-radius: 1rem;
+box-shadow: 5px 5px 10px var(--primary);
+`
+
+export default MainSt
