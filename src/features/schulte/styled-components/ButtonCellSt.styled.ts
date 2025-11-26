62b4a1116e15c6584a63a68b30687ab8ceeaf6d4
Vladimir Stoikov <vspsmoke@gmail.com>
2025-11-26 20:43:39 +0300

    ButtonCellSt.styled.ts(fix): issue with miltiple creation classes

--- src/features/schulte/styled-components/ButtonCellSt.styled.ts
+++ src/features/schulte/styled-components/ButtonCellSt.styled.ts
@@ -1,8 +1,12 @@
 import styled from "styled-components";
 
-const ButtonCellSt = styled.button<{ $backColor?: string }>`
-background-color: ${props => props.$backColor || 'white'};
-/* background-color: red; */
+const ButtonCellSt = styled.button.attrs<{ $backColor?: string }>(props => ({
+  style: {
+    backgroundColor: props.$backColor ?? 'white'
+  }
+}))`
+  border: none;
+  cursor: pointer;
 `
 
 export default ButtonCellSt;
