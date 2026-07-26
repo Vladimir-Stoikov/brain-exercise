33589482e947fd73f46bf0f16c5fcc182a33b458
Vladimir Stoikov <vspsmoke@gmail.com>
2026-07-26 23:08:20 +0300

    feat(Socratic): add question generate on click

--- src/pages/Socratic.tsx
+++ src/pages/Socratic.tsx
@@ -4,20 +4,27 @@ import { SocraticLayout } from '../features/socratic-question/styled-components/
 import { SocraticAnswer } from '../features/socratic-question/styled-components/SocraticAnswer.styled';
 import { SocraticQuestion } from '../features/socratic-question/styled-components/SocraticQuestion.styled';
 import { SocraticControls } from '../features/socratic-question/styled-components/SocraticControls.styled';
+import { questions } from '../features/socratic-question/data/questions';
 
 export default function SocraticPage() {
   const [answer, setAnswer] = useState('');
+  const [question, setQuestion] = useState<string | boolean>(false);
+
+  function generateQuestion() {
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    setQuestion(questions[randomIndex].text);
+  }
 
   return (
     <SocraticLayout>
       <h2>Socratic Question</h2>
 
-      <SocraticQuestion>Press Start to begin your Socratic dialogue.</SocraticQuestion>
+      {!question ? <SocraticQuestion>Press Start to begin your Socratic dialogue.</SocraticQuestion> : <SocraticQuestion>{question}</SocraticQuestion>}
 
       <SocraticAnswer value={answer} onChange={e => setAnswer(e.target.value)} placeholder='Your answer...' />
 
       <SocraticControls>
-        <ButtonSt>Start</ButtonSt>
+        <ButtonSt onClick={generateQuestion}>Start</ButtonSt>
       </SocraticControls>
     </SocraticLayout>
   );
